abf652f893533c23188cd54abe84945806f731a2
Policherla, Chaitanya Kumar <chaitanya-kumar.policherla@metlife.com>
2020-09-12 17:26:10 +0530

    sign in with email and password implemented

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import "./sign-in.styles.scss";
 import FormInput from "../FormInput/FormInput.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.util";
+import { auth, signInWithGoogle } from "../../firebase/firebase.util";
 
 class SignIn extends Component {
   constructor() {
@@ -20,9 +20,20 @@ class SignIn extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const {email, password} = this.state;
+    try{
+        await auth.signInWithEmailAndPassword(email, password);
+        this.setState({
+          email: "",
+          password: "",
+        });
+    }
+    catch(error){
+        console.log('error while sign in', error);
+    }
+    //this.setState({ email: "", password: "" });
   };
 
   render() {
